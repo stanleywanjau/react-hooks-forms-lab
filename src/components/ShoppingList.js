3bed468842f1a8768bf64f1466080a97f8c1daeb
stanleywanjau <stanleywanjau371@gmail.com>
2023-11-11 10:14:49 +0300

    completed assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,27 +1,49 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
-
+  const [searchText,setSearchText] = useState("");
+  const [filteredItems, setFilteredItems] = useState([]);
+    // State to manage the list of items
+    const [allItems, setAllItems] = useState(items);
+    useEffect(() => {
+      filterItems();
+    }, [selectedCategory, searchText, allItems]);
+  
+  
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
+  
+  function searchItem(query){
+    
+    
+    setSearchText(query);
+  }
+    // Function to add a new item to the list
+    function handleItemFormSubmit(newItem) {
+      setAllItems((prevItems) => [...prevItems, newItem]);
+    }
+  function filterItems() {
+    const filtered = items.filter((item) => {
+      const byCategory = selectedCategory === "All" || item.category === selectedCategory;
+      const bySearchText = item.name.toLowerCase().includes(searchText.toLowerCase());
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+      return byCategory && bySearchText;
+    });
 
-    return item.category === selectedCategory;
-  });
+    setFilteredItems(filtered);
+  }
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+       <ItemForm onItemFormSubmit={handleItemFormSubmit} />
+      <Filter search={searchText} onCategoryChange={handleCategoryChange} onSearchChange={searchItem}/>
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
